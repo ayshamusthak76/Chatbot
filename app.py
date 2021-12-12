d22355125cf3db3aa063ca1fab6c5d81730e54bd
ayshamusthak76 <74774965+ayshamusthak76@users.noreply.github.com>
2021-12-13 02:34:32 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -2,15 +2,27 @@
 import os
 import math
 import datetime
-
+import re
+import io
 import sys
 sys.path
-
-
+import zlib
+from werkzeug.utils import secure_filename
+from flask import Response
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session ,url_for
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+import face_recognition
+from PIL import Image
+from base64 import b64encode, b64decode
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
-
+from helpers import apology, login_required
 import tensorflow 
 from tensorflow import keras
 
@@ -122,22 +134,12 @@ import textwrap
 # Wrap text to 80 characters.
 wrapper = textwrap.TextWrapper(width=80) 
 
-# bert_abstract = "We introduce a new language representation model called BERT, which stands for Bidirectional Encoder Representations from Transformers. Unlike recent language representation models (Peters et al., 2018a; Radford et al., 2018), BERT is designed to pretrain deep bidirectional representations from unlabeled text by jointly conditioning on both left and right context in all layers. As a result, the pre-trained BERT model can be finetuned with just one additional output layer to create state-of-the-art models for a wide range of tasks, such as question answering and language inference, without substantial taskspecific architecture modifications. BERT is conceptually simple and empirically powerful. It obtains new state-of-the-art results on eleven natural language processing tasks, including pushing the GLUE score to 80.5% (7.7% point absolute improvement), MultiNLI accuracy to 86.7% (4.6% absolute improvement), SQuAD v1.1 question answering Test F1 to 93.2 (1.5 point absolute improvement) and SQuAD v2.0 Test F1 to 83.1 (5.1 point absolute improvement)."
-
-# print(wrapper.fill(bert_abstract))
-
-# question = "What does the 'B' in BERT stand for?"
-
-# answer_question(question, bert_abstract)
-
-
 intents = json.loads(open('D:/Aysha/sem 7/pbl/ChatbotB/ChatbotB/bert_ansrs.json').read())
 
 tokenizer = FullTokenizer(vocab_file=os.path.join(bert_ckpt_dir, "vocab.txt"))
 train = pd.read_csv("D:/Aysha/sem 7/pbl/ChatbotB/ChatbotB/train.csv")
 classes = train.intent.unique().tolist()
 # classes = bert_ansrs['intents']
-
 print(classes)
 def predict_class2(msg,model):
     print((msg))
@@ -170,7 +172,6 @@ def getResponse2(ints, intents_json, msg):
 
 def chatbot_response(msg):    
     ints = predict_class2(msg, model)
-    # print("INTS: ",ints)
     res = getResponse2(ints, intents,msg)
     return res
 
@@ -179,14 +180,266 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.static_folder = 'static'
 
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+# @app.route("/get")
+# def get_bot_response():
+#     userText = request.args.get('msg')
+#     return chatbot_response(userText)
+
+#starto
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Custom filter
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///D:/Aysha/sem 7/pbl/ChatbotB/ChatbotB/users.db")
+
 @app.route("/")
+@login_required
 def home():
-    return render_template("index.html")
+    return redirect("/home")
+
+@app.route("/home")
+@login_required
+def index():
+    return render_template("index copy.html")
 
 @app.route("/get")
 def get_bot_response():
+    print("AM in GET")
     userText = request.args.get('msg')
     return chatbot_response(userText)
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Assign inputs to variables
+        input_username = request.form.get("username")
+        input_password = request.form.get("password")
+
+        # Ensure username was submitted
+        if not input_username:
+            return render_template("login.html",messager = 1)
+
+
+
+        # Ensure password was submitted
+        elif not input_password:
+             return render_template("login.html",messager = 2)
+
+        # Query database for username
+        username = db.execute("SELECT * FROM users WHERE username = :username",
+                              username=input_username)
+
+        # Ensure username exists and password is correct
+        if len(username) != 1 or not check_password_hash(username[0]["hash"], input_password):
+            return render_template("login.html",messager = 3)
+
+        # Remember which user has logged in
+        session["user_id"] = username[0]["id"]
+
+
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Assign inputs to variables
+        input_username = request.form.get("username")
+        input_password = request.form.get("password")
+        input_confirmation = request.form.get("confirmation")
+
+        # Ensure username was submitted
+        if not input_username:
+            return render_template("register.html",messager = 1)
+
+        # Ensure password was submitted
+        elif not input_password:
+            return render_template("register.html",messager = 2)
+
+        # Ensure passwsord confirmation was submitted
+        elif not input_confirmation:
+            return render_template("register.html",messager = 4)
+
+        elif not input_password == input_confirmation:
+            return render_template("register.html",messager = 3)
+
+        # Query database for username
+        username = db.execute("SELECT username FROM users WHERE username = :username",
+                              username=input_username)
+
+        # Ensure username is not already taken
+        if len(username) == 1:
+            return render_template("register.html",messager = 5)
+
+        # Query database to insert new user
+        else:
+            new_user = db.execute("INSERT INTO users (username, hash) VALUES (:username, :password)",
+                                  username=input_username,
+                                  password=generate_password_hash(input_password, method="pbkdf2:sha256", salt_length=8),)
+
+            if new_user:
+                # Keep newly registered user logged in
+                session["user_id"] = new_user
+
+            # Flash info for the user
+            flash(f"Registered as {input_username}")
+
+            # Redirect user to homepage
+            return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+
+
+
+@app.route("/facereg", methods=["GET", "POST"])
+def facereg():
+    session.clear()
+    if request.method == "POST":
+
+
+        encoded_image = (request.form.get("pic")+"==").encode('utf-8')
+        username = request.form.get("name")
+        name = db.execute("SELECT * FROM users WHERE username = :username",
+                        username=username)
+              
+        if len(name) != 1:
+            return render_template("camera.html",message = 1)
+
+        id_ = name[0]['id']    
+        compressed_data = zlib.compress(encoded_image, 9) 
+        
+        uncompressed_data = zlib.decompress(compressed_data)
+        
+        decoded_data = b64decode(uncompressed_data)
+        
+        new_image_handle = open('./static/face/unknown/'+str(id_)+'.jpg', 'wb')
+        
+        new_image_handle.write(decoded_data)
+        new_image_handle.close()
+        try:
+            image_of_bill = face_recognition.load_image_file(
+            './static/face/'+str(id_)+'.jpg')
+        except:
+            return render_template("camera.html",message = 5)
+
+        bill_face_encoding = face_recognition.face_encodings(image_of_bill)[0]
+
+        unknown_image = face_recognition.load_image_file(
+        './static/face/unknown/'+str(id_)+'.jpg')
+        try:
+            unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+        except:
+            return render_template("camera.html",message = 2)
+
+
+#  o    mpare faces
+        results = face_recognition.compare_faces(
+        [bill_face_encoding], unknown_face_encoding)
+
+        if results[0]:
+            username = db.execute("SELECT * FROM users WHERE username = :username",
+                              username="swa")
+            session["user_id"] = username[0]["id"]
+            return redirect("/")
+        else:
+            return render_template("camera.html",message=3)
+
+
+    else:
+        return render_template("camera.html")
+
+
+
+@app.route("/facesetup", methods=["GET", "POST"])
+def facesetup():
+    if request.method == "POST":
+
+
+        encoded_image = (request.form.get("pic")+"==").encode('utf-8')
+
+
+        id_=db.execute("SELECT id FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["id"]
+        # id_ = db.execute("SELECT id FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["id"]    
+        compressed_data = zlib.compress(encoded_image, 9) 
+        
+        uncompressed_data = zlib.decompress(compressed_data)
+        decoded_data = b64decode(uncompressed_data)
+        
+        new_image_handle = open('./static/face/'+str(id_)+'.jpg', 'wb')
+        
+        new_image_handle.write(decoded_data)
+        new_image_handle.close()
+        image_of_bill = face_recognition.load_image_file(
+        './static/face/'+str(id_)+'.jpg')    
+        try:
+            bill_face_encoding = face_recognition.face_encodings(image_of_bill)[0]
+        except:    
+            return render_template("face.html",message = 1)
+        return redirect("/home")
+
+    else:
+        return render_template("face.html")
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return render_template("error.html",e = e)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
+
+#end
 if __name__ == "__main__":
     app.run()
